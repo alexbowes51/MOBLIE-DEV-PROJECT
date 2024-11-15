fb7a1152a15d62f53ab347efb534b6cd696cc342
alexbowes51 <alexbowes12@outlook.ie>
2024-11-15 10:50:29 +0000

    changing main.cpp

--- src/main.c
+++ src/main.c
@@ -24,50 +24,50 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 
 */
 
-#include "raylib.h"
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
+//#include "raylib.h"
+//#define RAYGUI_IMPLEMENTATION
+//#include "raygui.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+//#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-int main ()
-{
+//int main ()
+//{
 	// Tell the window to use vysnc and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	//SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	//InitWindow(1280, 800, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	//SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	//Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
 	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
+	//while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	//{
 		// drawing
-		BeginDrawing();
+		//BeginDrawing();
 
 		// Setup the backbuffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+		//ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		//DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
+		//EndDrawing();
+	//}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	//UnloadTexture(wabbit);
 
 	// destory the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
-}
+	//CloseWindow();
+	//return 0;
+//}
